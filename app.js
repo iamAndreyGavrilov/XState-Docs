0460013a826ddc7d904aeaa8368682f5769244ac
Andrey Gavrilov <boombardir20@gmail.com>
2022-05-06 11:41:02 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,75 @@
+import { createMachine } from "xstate";
+
+const promiseMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QBED2UB0BBA7gQwGswMBlAGzDAAcBPAYgBlUBXCAAgDtUBLAYzESgqqWNwAu3VB0EgAHogCMAViUYFATnUAWAOxKATDoDMABkMAOfQBoQNREoUYdOk0fXmd6hTv0A2LeZKAL5BNmiYuIRgdABieGRksGx4sBTUMsKiElIy8ggAtEbmWhiaWiYq6v5GbuY2dgUa6mpG+vomvtrmZgpGwaEg4diplFSMLOwAcjz8GSLiktJIcoh+GEXG-up9Pubm6jr1iPla+qptRjr+vka+yq4hYejDaWMkALZgCUkAZqioEDmWUWuWODnWLjuCl6zhc0KOBUuOicWgOOkCzl8Sh8-QGXAgcBkQ0iRFIrwaQnm2SWoDy+j6pX0WhuRm8JgUN2stmOvmRSlMAT6VXp0P0j0GzxJxBIvDwACdIECFjllnl8sp9BgtAoLPzzL59fo6tyEByStqDfqjFpmfoNOLifgiErqaDGlctTr0XqDb4jQj1eUMPyTAFLJ0bXtzA7JSN0stMsqaSsCuVNZYQ94VH62gGtKynPSrh5WroFFoY1AXSDVcdTr4MBnTFmlDmuQ18kpQ4WkdaKgavBWQkEgA */
+  createMachine({
+    id: "Dog",
+    initial: "Asleep",
+    states: {
+      Awake: {
+        initial: "Sleepy",
+        states: {
+          Sleepy: {
+            on: {
+              "Loud noice": {
+                target: "Scared",
+              },
+            },
+          },
+          Scared: {},
+        },
+        on: {
+          "Falls asleep": {
+            target: "Asleep",
+          },
+        },
+      },
+      Asleep: {
+        on: {
+          "Loud Noice": {
+            target: "#Dog.Awake.Scared",
+          },
+          "Smells food": {
+            target: "#Dog.Awake.Sleepy",
+          },
+        },
+      },
+    },
+  });
+
+const machine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QHcCGAbA1gOjQSwBc8A7KAYnTFQDcwACACwHsBbMRUABydkLyeIcQAD0QBaAIwAWAKzYADAHYAHBImKATBPkA2RTICcUgDQgAnog0bsOgMwqDuqfPkGDGzQF9PptFmwCdKh0fphkMAR0AEaoAMaYjKzsSCDcvEQCQqIIkjI62BIGOsoymrYaBrYyEqYWCBryEtgGMkpt6lYGEsrePiDETBBwQqG4qHykQml8mSnZYra28gV6yq46Og0bOrWIEvkS9srHS7ZSjrbu3r4YOIHBoVM8M4JziLbYyiqyis6KZR4pIpdggJBoPjIpM55DJLgZ-osJNcQKNQnQwMQhhAnul+K9QPMzlJsJCNMoNFJCrCpLZlCClstzoZYYodJCZF9kY8UtMMviROJbMUSVIyRSqWdaSCxHlPpttBUtLC1LZep4gA */
+  createMachine({
+    initial: "waiting",
+    states: {
+      waiting: {
+        on: {
+          "leave home": {
+            target: "on a walk",
+          },
+        },
+      },
+      "on a walk": {
+        on: {
+          "get back home": {
+            target: "walk ended",
+          },
+        },
+      },
+      "walk ended": {
+        type: "final",
+      },
+    },
+    id: "walk",
+  });
+
+const machine2 =
+  /** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgGEBDAGxIAIBbIgYwAsBLDMAOmYHdzYAXI7scgEZ8IAA5ZYDbgywYRAD0QBaAEwA2AJwsArAGZB2gCwrtg3QA41aw+gCeyweYDsLA+bNG1jlbu0rkaCDEZFS0jMwi4pLSsgrKKhq6OvpGJmaW1nbxagAMrtruhjkeOYaC6gEBQA */
+  createMachine({
+    //не имеет начального состояния
+    type: "parallel",
+    states: {
+      "new state 1": {},
+    },
+    id: "Call machine",
+  });
